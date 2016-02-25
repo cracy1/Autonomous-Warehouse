eb1b1b87971de3956d922865a9d03676c65c7692
Dominic Trott <dst512@cca-lg36-0905.cs.bham.ac.uk>
2016-02-25 12:35:21 +0000

    Refined requestItem, now returns int depending on success of picking up items

--- aw-rob/src/aw/robot/Display.java
+++ aw-rob/src/aw/robot/Display.java
@@ -13,31 +13,43 @@ public class Display {
 	private int amount;
 	private int screenHeight;
 	private int screenWidth;
-	private Graphics g;
+	private Graphics g = new Graphics();
 	
 	public Display(){
 		screenHeight = LCD.SCREEN_HEIGHT;
 		screenWidth = LCD.SCREEN_WIDTH;
-		Graphics g = new Graphics();
 		
 	}
 	
-	public void requestItem(String item,int amount){
+	public int requestItem(String item,int amount){
 		this.item = item;
 		this.amount = amount;
 		
 		for(int i = 0; i < amount; i++){
+			g.setFont(Font.getDefaultFont());
 			g.drawString("I need ", screenWidth/2, 0, Graphics.HCENTER);
 			
 			g.setFont(Font.getLargeFont());
-			g.drawString(Integer.toString(amount - i), screenWidth/2, screenHeight/4, Graphics.HCENTER, true);
+			g.drawString(Integer.toString(amount - i), screenWidth/2, screenHeight/8, Graphics.HCENTER, true);
 			
 			g.setFont(Font.getDefaultFont());
-			g.drawString("more " + item, screenWidth/2, screenHeight/2, Graphics.HCENTER);
+			g.drawString("more " + item, screenWidth/2, screenHeight*3/8, Graphics.HCENTER);
 			
-			Button.ENTER.waitForPress();
-			g.clear();
+			g.setFont(Font.getSmallFont());
+			g.drawString("If", screenWidth/2, screenHeight*7/10, Graphics.HCENTER);
+			g.drawString("I am in the wrong place", screenWidth/2, screenHeight*8/10, Graphics.HCENTER);
+			g.drawString("press the escape button", screenWidth/2, screenHeight*9/10, Graphics.HCENTER);
+			
+			int buttonPress = Button.waitForAnyPress();
+			if(buttonPress == Button.ID_ESCAPE){
+				return 0;
+			}
+			else{
+				g.clear();
+				continue;
+			}
 		}
 		Sound.systemSound(false, 2);
+		return 1;
 	}
 }
