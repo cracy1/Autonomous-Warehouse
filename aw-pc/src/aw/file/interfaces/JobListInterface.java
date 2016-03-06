e5961f8cd28d8904f6860e8c459d6e2006dd00cc
cracy1 <andrei.n.craciun10@gmail.com>
2016-03-06 16:47:59 +0000

    Modified to fit the methods

--- aw-pc/src/aw/file/interfaces/JobListInterface.java
+++ aw-pc/src/aw/file/interfaces/JobListInterface.java
@@ -6,5 +6,5 @@ import aw.robotics.Robot;
 public interface JobListInterface {
 	public String getJob(int i); 
 	public Job setJob(Robot rob); //robot object.
-	public void sort();
+	//public void sort();
 }
