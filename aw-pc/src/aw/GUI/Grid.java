4a344ed85a45c875113ab3c6a9bc165093dd5d7d
dst512 <dst512@cs.bham.ac.uk>
2016-03-10 12:31:27 +0000

    Path visualization now supports 3 robots

--- aw-pc/src/aw/GUI/Grid.java
+++ aw-pc/src/aw/GUI/Grid.java
@@ -25,11 +25,15 @@ public class Grid extends JPanel{
 	private ArrayList<Line2D.Double> yLines; 
 	
 
-	private int recWidth = 30;
-	private int recHeight = 30;
-	private int recCenterX = 50;
-	private int recCenterY = 100;
-	private int height,width, xSize, ySize, recX, recY, xConvert, yConvert;
+	private int robotWidth = 30;
+	private int robotHeight = 30;
+	private int robot1CenterX = 50;
+	private int robot1CenterY = 100;
+	private int robot2CenterX = 600;
+	private int robot2CenterY = 100;
+	private int robot3CenterX = 50;
+	private int robot3CenterY = 400;
+	private int height,width, xSize, ySize, robot1X, robot1Y, robot2X, robot2Y, robot3X, robot3Y, xConvert, yConvert;
 	private GridMap grid;
 	
 	private ArrayList<Point> route1;
@@ -44,12 +48,14 @@ public class Grid extends JPanel{
 		this.height = 800;
 		this.width = 800;
 		route1 = new ArrayList<Point>();
+		route2 = new ArrayList<Point>();
+		route3 = new ArrayList<Point>();
 		
 		
-//Square just goes around the perimeter of the grid for the moment
-//Just tester code for the moment.
-		recX = recCenterX - (recWidth /2);
-		recY = recCenterY - (recHeight/2);
+		//Square just goes around the perimeter of the grid for the moment
+		//Just tester code for the moment.
+		robot1X = robot1CenterX - (robotWidth /2);
+		robot1Y = robot1CenterY - (robotHeight/2);
 		ActionListener movingRec = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 //				if(recCenterY == 100 && recCenterX >= 50 && recCenterX < 50 * xSize) {
@@ -77,50 +83,133 @@ public class Grid extends JPanel{
 //					recY = recCenterY - (recHeight/2);
 //					repaint();
 //				}
+				
+				//Draws the location of the first robot along its given route
 				if(!route1.isEmpty()){
-					
 					Point nextCoord = route1.get(0);
-					if(recCenterX < nextCoord.getX()*50 + 50){
-						recCenterX++;
-						recX = recCenterX - (recWidth /2);
-						recY = recCenterY - (recHeight/2);
+					if(robot1CenterX < nextCoord.getX()*50 + 50){
+						robot1CenterX++;
+						robot1X = robot1CenterX - (robotWidth /2);
+						robot1Y = robot1CenterY - (robotHeight/2);
 						
 						repaint();
 					}
-					else if(recCenterX > nextCoord.getX()*50 + 50){
-						recCenterX--;
-						recX = recCenterX - (recWidth /2);
-						recY = recCenterY - (recHeight/2);
+					else if(robot1CenterX > nextCoord.getX()*50 + 50){
+						robot1CenterX--;
+						robot1X = robot1CenterX - (robotWidth /2);
+						robot1Y = robot1CenterY - (robotHeight/2);
 						
 						repaint();
 					}
-					else if(recCenterY < nextCoord.getY()*50 + 100){
-						recCenterY++;
-						recX = recCenterX - (recWidth /2);
-						recY = recCenterY - (recHeight/2);
+					else if(robot1CenterY < nextCoord.getY()*50 + 100){
+						robot1CenterY++;
+						robot1X = robot1CenterX - (robotWidth /2);
+						robot1Y = robot1CenterY - (robotHeight/2);
 						
 						repaint();
 					}
-					else if(recCenterY > nextCoord.getY()*50 + 100){
-						recCenterY--;
-						recX = recCenterX - (recWidth /2);
-						recY = recCenterY - (recHeight/2);
+					else if(robot1CenterY > nextCoord.getY()*50 + 100){
+						robot1CenterY--;
+						robot1X = robot1CenterX - (robotWidth /2);
+						robot1Y = robot1CenterY - (robotHeight/2);
 						
 						repaint();
 					}
 					//Removes nodes of the route that the robot has passed
-					if((int)route1.get(0).getX()*50 + 50 == recCenterX && (int)route1.get(0).getY()*50 + 100 == recCenterY){
+					if((int)route1.get(0).getX()*50 + 50 == robot1CenterX && (int)route1.get(0).getY()*50 + 100 == robot1CenterY){
 						route1.remove(0);
+					}
+				}
+				
+				//Draws the location of the second robot along its given route
+				if(!route2.isEmpty()){
+					Point nextCoord = route2.get(0);
+					if(robot2CenterX < nextCoord.getX()*50 + 50){
+						robot2CenterX++;
+						robot2X = robot2CenterX - (robotWidth /2);
+						robot2Y = robot2CenterY - (robotHeight/2);
+						
+						repaint();
+					}
+					else if(robot2CenterX > nextCoord.getX()*50 + 50){
+						robot2CenterX--;
+						robot2X = robot2CenterX - (robotWidth /2);
+						robot2Y = robot2CenterY - (robotHeight/2);
+						
+						repaint();
+					}
+					else if(robot2CenterY < nextCoord.getY()*50 + 100){
+						robot2CenterY++;
+						robot2X = robot2CenterX - (robotWidth /2);
+						robot2Y = robot2CenterY - (robotHeight/2);
+						
+						repaint();
+					}
+					else if(robot2CenterY > nextCoord.getY()*50 + 100){
+						robot2CenterY--;
+						robot2X = robot2CenterX - (robotWidth /2);
+						robot2Y = robot2CenterY - (robotHeight/2);
+						
+						repaint();
+					}
+					//Removes nodes of the route that the robot has passed
+					if((int)route2.get(0).getX()*50 + 50 == robot2CenterX && (int)route2.get(0).getY()*50 + 100 == robot2CenterY){
+						route2.remove(0);
+					}
+				}
+				
+				//Draws the location of the third robot along its given route
+				if(!route3.isEmpty()){
+					Point nextCoord = route3.get(0);
+					if(robot3CenterX < nextCoord.getX()*50 + 50){
+						robot3CenterX++;
+						robot3X = robot3CenterX - (robotWidth /2);
+						robot3Y = robot3CenterY - (robotHeight/2);
+						
+						repaint();
+					}
+					else if(robot3CenterX > nextCoord.getX()*50 + 50){
+						robot3CenterX--;
+						robot3X = robot3CenterX - (robotWidth /2);
+						robot3Y = robot3CenterY - (robotHeight/2);
+						
+						repaint();
+					}
+					else if(robot3CenterY < nextCoord.getY()*50 + 100){
+						robot3CenterY++;
+						robot3X = robot3CenterX - (robotWidth /2);
+						robot3Y = robot3CenterY - (robotHeight/2);
+						
+						repaint();
+					}
+					else if(robot3CenterY > nextCoord.getY()*50 + 100){
+						robot3CenterY--;
+						robot3X = robot3CenterX - (robotWidth /2);
+						robot3Y = robot3CenterY - (robotHeight/2);
+						
+						repaint();
+					}
+					//Removes nodes of the route that the robot has passed
+					if((int)route3.get(0).getX()*50 + 50 == robot3CenterX && (int)route3.get(0).getY()*50 + 100 == robot3CenterY){
+						route3.remove(0);
 					}
 				}
 			}
 		};
-//Updates the position of the red square every 20 milliseconds.		
+		//Updates the position of the red square every 20 milliseconds.		
 		Timer timer = new Timer(20, movingRec);
 		
 		timer.start();
-		
-		
+	}
+	
+	private void drawRoute(ArrayList<Point> route, int robotCenterX, int robotCenterY, Graphics2D g2, Color color){
+		g2.setStroke(new BasicStroke(8));
+		g2.setColor(color);
+		g2.drawLine(robotCenterX, robotCenterY, (int)(50 + route.get(0).getX()*50), (int)(100 + route.get(0).getY()*50));
+		for(int i = 0; i < route.size() - 1; i++){
+			g2.drawLine((int)(50 + route.get(i).getX()*50), (int)(100 + route.get(i).getY()*50), (int)(50 + route.get(i+1).getX()*50), (int)(100 + route.get(i+1).getY()*50));
+		}
+		g2.drawOval((int)(50 + route.get(route.size() - 1).getX()*50) - 5, (int)(95 + route.get(route.size() - 1).getY()*50), 10, 10);
 	}
 	
 	public void draw(Graphics g) {
@@ -144,45 +233,56 @@ public class Grid extends JPanel{
 		
 		//Drawing route of the robot
 		if(!route1.isEmpty()){
-			g2.setStroke(new BasicStroke(8));
-			g2.setColor(Color.RED);
-			g2.drawLine(recCenterX, recCenterY, (int)(50 + route1.get(0).getX()*50), (int)(100 + route1.get(0).getY()*50));
-			for(int i = 0; i < route1.size() - 1; i++){
-				g2.drawLine((int)(50 + route1.get(i).getX()*50), (int)(100 + route1.get(i).getY()*50), (int)(50 + route1.get(i+1).getX()*50), (int)(100 + route1.get(i+1).getY()*50));
-			}
-			g2.drawOval((int)(50 + route1.get(route1.size() - 1).getX()*50) - 5, (int)(95 + route1.get(route1.size() - 1).getY()*50), 10, 10);
+			drawRoute(route1, robot1CenterX, robot1CenterY, g2, Color.RED);
+		}
+		if(!route2.isEmpty()){
+			drawRoute(route2, robot2CenterX, robot2CenterY, g2, Color.BLUE);
+		}
+		if(!route3.isEmpty()){
+			drawRoute(route3, robot3CenterX, robot3CenterY, g2, Color.GREEN);
 		}
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D)g;
-//makes the square moving a lot smoother.
+		//makes the square moving a lot smoother.
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		draw(g2);
 		float thickness = 5;
 		Stroke oldStroke = g2.getStroke();
 		g2.setStroke(new BasicStroke(thickness));
 		
-		
 		g2.setColor(Color.RED);
-		g2.drawRect(recX, recY, recWidth, recHeight);
+		g2.drawRect(robot1X, robot1Y, robotWidth, robotHeight);
+		
+		g2.setColor(Color.BLUE);
+		g2.drawRect(robot2X, robot2Y, robotWidth, robotHeight);
+		
+		g2.setColor(Color.GREEN);
+		g2.drawRect(robot3X, robot3Y, robotWidth, robotHeight);
 	}
 	
 	//Sets the routes of the different robots to a locally stored variable
 	public void setRoute(ArrayList<Point> route, String robot){
 		if(robot.equals("Ricardo")){
 			route1 = route;
-			recCenterX = (int)route1.get(0).getX()*50 + 50;
-			recCenterY = (int)route1.get(0).getY()*50 + 100;
+			robot1CenterX = (int)route1.get(0).getX()*50 + 50;
+			robot1CenterY = (int)route1.get(0).getY()*50 + 100;
 			route1.remove(0);
 			
 		}
 		else if(robot.equals("NXT")){
-			route2 = route;		
+			route2 = route;
+			robot2CenterX = (int)route2.get(0).getX()*50 + 50;
+			robot2CenterY = (int)route2.get(0).getY()*50 + 100;
+			route2.remove(0);
 		}
-		else{
+		else if(robot.equals("TheOtherRobot")){
 			route3 = route;
+			robot3CenterX = (int)route3.get(0).getX()*50 + 50;
+			robot3CenterY = (int)route3.get(0).getY()*50 + 100;
+			route3.remove(0);
 		}
 	}
 }
--- aw-pc/src/aw/test/GUITest.java
+++ aw-pc/src/aw/test/GUITest.java
@@ -12,16 +12,67 @@ public class GUITest {
 		GUI guiTest = new GUI();
 		Job job = new Job("10003,ah,1,bi,1");
 		guiTest.setJob(job, "Ricardo");
+		
+		//Setting route for Ricardo
 		Point p1 = new Point(0, 0);
 		Point p2 = new Point(0, 1);
 		Point p3 = new Point(1, 1);
 		Point p4 = new Point(2, 1);
+		Point p5 = new Point(3, 1);
+		Point p6 = new Point(3, 2);
+		Point p7 = new Point(3, 3);
+		Point p8 = new Point(3, 4);
 		ArrayList<Point> route = new ArrayList<Point>();
 		route.add(p1);
 		route.add(p2);
 		route.add(p3);
 		route.add(p4);
+		route.add(p5);
+		route.add(p6);
+		route.add(p7);
+		route.add(p8);
 		guiTest.setRoute(route, "Ricardo");
+		
+		//Setting route for NXT
+		Point p9 = new Point(0, 7);
+		Point p10 = new Point(1, 7);
+		Point p11 = new Point(2, 7);
+		Point p12 = new Point(3, 7);
+		Point p13 = new Point(4, 7);
+		Point p14 = new Point(5, 7);
+		Point p15 = new Point(5, 6);
+		Point p16 = new Point(5, 5);
+		ArrayList<Point> route2 = new ArrayList<Point>();
+		route2.add(p9);
+		route2.add(p10);
+		route2.add(p11);
+		route2.add(p12);
+		route2.add(p13);
+		route2.add(p14);
+		route2.add(p15);
+		route2.add(p16);
+		guiTest.setRoute(route2, "NXT");
+		
+		//Setting route for The Other Robot
+		Point p17 = new Point(11, 0);
+		Point p18 = new Point(11, 1);
+		Point p19 = new Point(11, 2);
+		Point p20 = new Point(11, 3);
+		Point p21 = new Point(11, 4);
+		Point p22 = new Point(11, 5);
+		Point p23 = new Point(11, 6);
+		Point p24 = new Point(11, 7);
+		ArrayList<Point> route3 = new ArrayList<Point>();
+		route3.add(p17);
+		route3.add(p18);
+		route3.add(p19);
+		route3.add(p20);
+		route3.add(p21);
+		route3.add(p22);
+		route3.add(p23);
+		route3.add(p24);
+		guiTest.setRoute(route3, "TheOtherRobot");		
+		
 	}
 
 }
