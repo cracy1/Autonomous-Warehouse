2b830aa8ab78a02c6e17092e03864d3bf8d51dac
cracy1 <andrei.n.craciun10@gmail.com>
2016-03-14 14:24:58 +0000

    Started working on TSP

--- aw-pc/src/aw/file/Job.java
+++ aw-pc/src/aw/file/Job.java
@@ -1,6 +1,10 @@
 package aw.file;
 
+import java.util.ArrayList;
+
 import aw.file.interfaces.JobInterface;
+
+// try to extend Comparable
 
 public class Job implements JobInterface {
 
@@ -9,6 +13,7 @@ public class Job implements JobInterface {
 	private int JobID;
 	private int counter;
 	private ItemList item = new ItemList();
+	private int index;
 
 	public Job(String row) {
 		JobID = Integer.parseInt(row.split(",")[0]);
@@ -59,8 +64,8 @@ public class Job implements JobInterface {
 		return null;
 	}
 
-// to be modified	
-	
+	// to be modified
+
 	@Override
 	public void sort() {
 		for (int i = 0; i < items.length - 1; i++) {
@@ -120,4 +125,93 @@ public class Job implements JobInterface {
 
 	}
 
-}
+	public void sortItems(int xCoord, int yCoord) {
+		ArrayList<String> res = new ArrayList<String>();
+		ArrayList<String> P = new ArrayList<String>();
+		for (int i = 0; i < numberItems(); i++) {
+			P.add(items[i]);
+		}
+		System.out.println("Initial P = " + P);
+		//int index;
+		int x = xCoord; int y = yCoord;
+//		int xTemp = -1, yTemp = -1;
+		int sizeOfP = P.size();
+		System.out.println("Initial size of P = " + sizeOfP);
+		while (sizeOfP != 0) {
+			int i = 0;
+			int minDist = Integer.MAX_VALUE;
+			if(!res.isEmpty()){
+				while(i < sizeOfP) {
+					minDist = Integer.MAX_VALUE;
+					ItemList item = new ItemList();
+					String[] temp = new String[res.size()];
+					for(int j = 0; j < res.size(); j++ ){
+						res.toArray(temp);
+						if (minDist > getDistance(item.getX(item.getIndex(temp[j])), item.getY(item.getIndex(temp[j])), item.getX(item.getIndex(items[i])),item.getY(item.getIndex(items[i])))) {
+							minDist = getDistance(item.getX(item.getIndex(temp[j])), item.getY(item.getIndex(temp[j])), item.getX(item.getIndex(items[i])), item.getY(item.getIndex(items[i])));
+							index = i;
+						}
+						System.out.println("items[j]:" + items[j]);
+					}
+					res.add(item.getName(item.getIndex(items[index])));
+					P.remove(index); 
+					P.trimToSize();
+					sizeOfP--;
+					i++;
+					System.out.println(P.size());
+					System.out.println("P: " + P);
+					System.out.println(res.size());
+					System.out.println("res: " + res);
+					System.out.println("item removed from P and added to res: " + items[index]);
+					System.out.println();
+					
+				}
+			}
+			else{
+				while(i < sizeOfP){
+					ItemList item = new ItemList();
+					if (minDist > getDistance(x, y, item.getX(item.getIndex(items[i])),item.getY(item.getIndex(items[i])))) {
+						minDist = getDistance(x, y, item.getX(item.getIndex(items[i])), item.getY(item.getIndex(items[i])));
+						index = i;
+						}
+					i++;
+				}
+				res.add(item.getName(item.getIndex(items[index])));
+				P.remove(index); 
+				P.trimToSize();
+				sizeOfP--;
+				System.out.println("res: " + res);
+			}
+				
+
+
+			}
+
+//			System.out.println(res.size());
+		}
+
+		// for (int i = 0; i < P.size(); i++) {
+		// ItemList item = new ItemList();
+		//
+		// }
+//	
+//	private String closest(String[] a){
+//		for(int i = 0; i < a.length; i ++){
+//		return null;
+//		}
+	
+	}
+
+
+	// public void tsp(int[] arr ){
+	// int i = 0;
+	//
+	//
+	//
+	// for(i = 0; i < arr.length; i++){
+	//
+	//
+	// }
+	// }
+
+
