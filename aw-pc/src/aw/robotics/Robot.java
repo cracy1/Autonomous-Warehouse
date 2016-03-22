26979a92db36e2928c282b420525f47c8bf4e23a
dst512 <dst512@cs.bham.ac.uk>
2016-03-22 13:13:42 +0000

    Jobs now display properly

--- aw-pc/src/aw/robotics/Robot.java
+++ aw-pc/src/aw/robotics/Robot.java
@@ -184,8 +184,8 @@ public class Robot implements BluetoothCommandListener, Runnable{
 	public void run() {
 		while(running){
 			if(jobs.size() > 0){
-				executeJob(jobs.getFirst());
-				gui.setJob(jobs.removeFirst(), name);
+				gui.setJob(jobs.getFirst(), this.name);
+				executeJob(jobs.removeFirst());
 			}
 		}
 	}
