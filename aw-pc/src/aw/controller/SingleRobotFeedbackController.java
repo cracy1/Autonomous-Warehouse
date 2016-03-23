2b54f12b41a277bd5b95b66c1f5c1f12fa4172e9
= <aranlong@live.co.uk>
2016-03-23 17:24:15 +0000

    Removed test code

--- aw-pc/src/aw/controller/SingleRobotFeedbackController.java
+++ aw-pc/src/aw/controller/SingleRobotFeedbackController.java
@@ -23,13 +23,7 @@ public class SingleRobotFeedbackController extends Controller{
 	}
 	
 	public static void main(String[] args){
-		JobList jobList = new JobList();
-		for(int i = 0; i < 100; i++){
-			Job job = new Job(jobList.getJob(i));
-			System.out.println(job);
-			//robots.getFirst().addJob(job);
-		}
-//		Communication.addRobots();
-//		new SingleRobotFeedbackController();
+		Communication.addRobots();
+		new SingleRobotFeedbackController();
 	}
 }
